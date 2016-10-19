432a7474ee39d215319bc69a0fd888cf1d6572f6
Tobias Rojahn <tobias@rojahn.org>
2016-10-19 13:48:14 +0200

    menu (buttons are still to big even after scaling them donw by 50%)

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -1,18 +1,21 @@
-define(['lib/scene', 'basic/button', 'core/game', 'geo/v2', 'transitions/slideinright', 'basic/morph', 'definition/easing', 'basic/layout', 'scenes/play'],
-	function(Scene, Button, game, V2, SlideInRightTransition, Morph, Easing, Layout, PlayScene) {
+define(['lib/scene', 'basic/button', 'core/game', 'geo/v2', 'transitions/slideinright', 'basic/morph', 'definition/easing', 'basic/layout', 'scenes/play', 'core/graphic'],
+	function(Scene, Button, game, V2, SlideInRightTransition, Morph, Easing, Layout, PlayScene, graphics) {
+		graphics.add('img/asset_start_button.png');
+		graphics.add('img/asset_credits_button.png');
+		graphics.add('img/bg.jpg');
+
 		function MenuScene() {
 			Scene.call(this);
+			this.bg = 'img/bg.jpg';
 
-			var playButton = Button.create(new V2(0, 680), function() { game.scene = new PlayScene(100); }).rect(280, 80).text("Play");
-			var creditsButton = Button.create(new V2(0, 680), function() { game.scene = new SlideInRightTransition(require('config/scenes').credits, 1000, Easing.OUTQUAD); }).rect(360, 80).text("Credits");
-			var helpButton = Button.create(new V2(0, 680), function() { game.scene = require('config/scenes').help; }).rect(300, 80).text("Help");
-			var particlesButton = Button.create(new V2(0, 680), function() { game.scene = require('config/scenes').particles; }).rect(300, 80).text("Particles");
+			var playButton = Button.create(new V2(0, 680), function() { game.scene = new PlayScene(100); }).img('img/asset_start_button.png', 1);
+			var creditsButton = Button.create(new V2(0, 680), function() { game.scene = require('config/scenes').credits}).img('img/asset_credits_button.png', 1);
+
+			//var helpButton = Button.create(new V2(0, 680), function() { game.scene = require('config/scenes').help; }).rect(300, 80).text("Help");
 
 			var vLayout = new Layout.vertical(new V2(0, 20), 20, 50);
 			vLayout.add(playButton);
 			vLayout.add(creditsButton);
-			vLayout.add(helpButton);
-			vLayout.add(particlesButton);
 			vLayout.align("center");
 			this.center(vLayout);
 
